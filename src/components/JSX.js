26b10cc1dcb388fecb8266565cef7e1029d91845
Sanskar Choubey <choubeysanskar03@gmail.com>
2021-01-18 16:28:09 +0530

    Welcome without the JSX

--- src/components/JSX.js
+++ src/components/JSX.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-function Message(){
-    return (
-            <h1>Welcome Sanskar</h1>
-    )
+// function Message(){
+//     return (
+//             <h1>Welcome Sanskar</h1>
+//     )
+// }
+
+const Message = () => {
+    return React.createElement('div',{id:'WelcomeMessage'},React.createElement('h1',null,'Welcome Sanskar'));
 }
 
 export default Message
